083b70b165a5e81dfaaacaff9417370b6c593df6
sjaehnig <sjaehnig@umich.edu>
2020-07-16 07:07:27 -0700

    styling changes

--- script.js
+++ script.js
@@ -1,7 +1,3 @@
-down_arrow_1.onclick = function(event) {
-  document.getElementById("information").scrollIntoView();
-}
-
 menu_icon.onclick = function(event) {
   if(document.getElementById('link_list').style.display != "block"){
     document.getElementById('link_list').style.display = "block";
@@ -10,30 +6,26 @@ menu_icon.onclick = function(event) {
   }
 }
 
-linkedin_icon.onclick = function(event) {
-  location.href = "https://www.linkedin.com/in/sam-jaehnig-3932b5195/";
-}
+// linkedin_icon.onclick = function(event) {
+//   location.href = "https://www.linkedin.com/in/sam-jaehnig-3932b5195/";
+// }
 
-twitter_icon.onclick = function(event) {
-  location.href = "https://twitter.com/samjaehnig";
-}
+// twitter_icon.onclick = function(event) {
+//   location.href = "https://twitter.com/samjaehnig";
+// }
 
-facebook_icon.onclick = function(event) {
-  location.href = "https://www.facebook.com/profile.php?id=100010288356446";
-}
+// facebook_icon.onclick = function(event) {
+//   location.href = "https://www.facebook.com/profile.php?id=100010288356446";
+// }
 
-email_icon.onclick = function(event) {
-  location.href = "mailto:sjaehnig@umich.edu";
-}
+// email_icon.onclick = function(event) {
+//   location.href = "mailto:sjaehnig@umich.edu";
+// }
 
-resume_icon.onclick = function(event) {
-  location.href = "resume.pdf";
-}
+// resume_icon.onclick = function(event) {
+//   location.href = "resume.pdf";
+// }
 
-instagram_icon.onclick = function(event) {
-  location.href = "https://www.instagram.com/sammmmy.j/";
-}
-
-back.onclick = function(event) {
-  location.href = "index.html";
-}
+// instagram_icon.onclick = function(event) {
+//   location.href = "https://www.instagram.com/sammmmy.j/";
+// }
