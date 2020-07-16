af17f2142438e316ce3448c4d6a7cc62222ab201
sjaehnig <sjaehnig@umich.edu>
2020-07-16 13:01:04 -0700

    new form

--- script.js
+++ script.js
@@ -5,27 +5,3 @@ menu_icon.onclick = function(event) {
     document.getElementById('link_list').style.display = "none";
   }
 }
-
-// linkedin_icon.onclick = function(event) {
-//   location.href = "https://www.linkedin.com/in/sam-jaehnig-3932b5195/";
-// }
-
-// twitter_icon.onclick = function(event) {
-//   location.href = "https://twitter.com/samjaehnig";
-// }
-
-// facebook_icon.onclick = function(event) {
-//   location.href = "https://www.facebook.com/profile.php?id=100010288356446";
-// }
-
-// email_icon.onclick = function(event) {
-//   location.href = "mailto:sjaehnig@umich.edu";
-// }
-
-// resume_icon.onclick = function(event) {
-//   location.href = "resume.pdf";
-// }
-
-// instagram_icon.onclick = function(event) {
-//   location.href = "https://www.instagram.com/sammmmy.j/";
-// }
