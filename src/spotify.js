d3a3e4c943cb5cbd009fd30746cf778ab66f213f
sjaehnig <sjaehnig@umich.edu>
2020-07-29 16:59:38 -0700

    fixed spotify bug

--- src/spotify.js
+++ src/spotify.js
@@ -17,23 +17,20 @@ router.get("/", (req, response) => {
     const client_secret = process.env.CLIENT_SECRET;  
     const refresh_token = process.env.REFRESH_TOKEN;
 
-    console.log("here");
     var authOptions = {
         url: 'https://accounts.spotify.com/api/token',
         headers: { 'Authorization': 'Basic ' + (new Buffer(client_id + ':' + client_secret).toString('base64')) },
         form: { grant_type: 'refresh_token', refresh_token: refresh_token }
     };
-    console.log("hello");
 
     request.post(authOptions, function(error, res) {
         var header = { 'Authorization': 'Bearer ' + (JSON.parse(res.body)).access_token };
         var song_name, artist, song_url;
-        request({ url: "https://api.spotify.com/v1/me/player/currently-playing", headers: header }, function(error, res, body) {
-            if((res.statusCode == 204)) {
+        request({ url: "https://api.spotify.com/v1/me/player/currently-playing?", headers: header }, function(error, res, body) {
+            if((res.statusCode == 204) || JSON.parse(body).currently_playing_type != "track") {
                 request({ url:"https://api.spotify.com/v1/me/player/recently-played?type=track&limit=1", headers: header }, function(error, res, body) {
                     var body_text = JSON.parse(body);
                     var track = body_text.items[0].track;
-                    console.log(song_name);
                     song_name = track.name;
                     artist = track.artists[0].name;
                     song_url = track.external_urls.spotify;
@@ -44,7 +41,6 @@ router.get("/", (req, response) => {
                 song_name = body_text.item.name;
                 artist = (body_text.item.artists)[0].name;
                 song_url = body_text.item.external_urls.spotify;
-                console.log(song_name);
                 response.json({ "song_name": song_name, "artist": artist, "song_url": song_url });
             }
         });
