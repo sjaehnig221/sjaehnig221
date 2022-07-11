d4dd9fe3a7407c980d5ca5d2230c26bbd5d84187
Sam Jaehnig <sjaehnig@umich.edu>
2022-07-11 11:22:32 -0700

    update website

--- script.js
+++
@@ -1,7 +0,0 @@
-menu_icon.onclick = function(event) {
-  if(document.getElementById('link_list').style.display != "block"){
-    document.getElementById('link_list').style.display = "block";
-  } else {
-    document.getElementById('link_list').style.display = "none";
-  }
-}
